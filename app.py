1314dc78cd9de0fc7d5920b49e9a944b1216bcd1
imxana <xana.awaken@gmail.com>
2016-09-19 15:26:31 +0800

    all

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, request, url_for
 app = Flask(__name__)
 
 import ec_forum.account as ac
+import ec_forum.salt as salt
 
 ac.run(app)
+salt.run(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
