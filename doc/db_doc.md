5b714fc470d43752fcca5e2173833973111b72f1
imxana <xana.awaken@gmail.com>
2016-09-23 15:20:02 +0800

    rep init

--- doc/db_doc.md
+++ doc/db_doc.md
@@ -1,16 +1,24 @@
 database
 
+## 这张表已经被废弃了！废弃了！废弃了！以 ./doc/schema.sql 为准！
+
 ## ec_user
 
 字段 | 字段 | 备注
 ------------ | ------------- | ------------
 u_id | 用户id | int 主键
 u_name | 用户名 | str 必填唯一
+u_psw text,
 u_email | 用户email | str 必填
 u_email_confirm | 用户email是否验证 | int
 u_level | 用户权限等级 | int
 u_reputation | 用户声望 | int
 u_realname | 用户真实姓名 | str，非必填
+u_blog text,
+u_github text,
+u_articles text,
+u_questions text,
+u_answers text,
 
 ## ec_question
 
@@ -19,8 +27,11 @@ u_realname | 用户真实姓名 | str，非必填
 q_id | 问题id | int 主键
 u_id | 提问用户id | int 外键
 q_title | 问题标题 | str 非空
+q_tag | 问题标签 | str
 q_text | 问题正文 | str 非空
-q_date | 提问日期 | date 
+q_date | 提问日期 | date
+q_close | 是否关闭 | int
+q_report | 被举报次数 | int
 
 ## ec_answer
 
@@ -29,8 +40,8 @@ q_date | 提问日期 | date
 a_id | 回答id | int 主键
 u_id | 回答用户id | int 外键
 a_text | 回答正文 | str 非空
-a_date | 回答日期 | date 
-a_reputation | 回答获得声望 | int
+a_date | 回答日期 | date
+a_like | 回答被赞数 | int
 
 ## ec_article
 
@@ -40,7 +51,7 @@ t_id | 文章id | int 主键
 u_id | 文章作者id | int 外键
 t_title | 文章标题 | str 非空
 t_text | 文章正文 | str 非空
-t_date | 文章发表日期 | date 
+t_date | 文章发表日期 | date
 t_reputation | 文章获得声望 | int
 
 ## ec_comment
@@ -48,10 +59,12 @@ t_reputation | 文章获得声望 | int
 字段 | 字段 | 备注
 ------------ | ------------- | ------------
 c_id | 评论id | int 主键
-t_id | 文章id | int 外键
+type | 评论类型 | q/a/t
+id | 对应事件id | int 外键
 u_id | 评论用户id | int 外键
 c_text | 评论正文 | str 非空
-a_date | 评论日期 | date 
+c_date | 评论日期 | date
+c_like | 评论被赞数 | int
 
 ## ec_reputaion
 
@@ -60,11 +73,5 @@ a_date | 评论日期 | date
 r_id | 声望变化id | 主键
 type | 声望类型 | q/a/t/c
 id | 对应事件id | int 外键
-ua_id | 评价用户id | int 外键 
-ub_id | 接受用户id | int 外键 
-
-
-
-
-
-
+ua_id | 评价用户id | int 外键
+ub_id | 接受用户id | int 外键
--- doc/schema.sql
+++ doc/schema.sql
@@ -5,66 +5,70 @@ use ec_forum;
 
 create table ec_user
 (
-    u_id integer primary key,
-    u_name text,
-    u_psw text,
-    u_email text,
-    u_email_confirm integer,
-    u_level integer,
-    u_reputation integer,
-    u_realname text,
-    u_blog text,
-    u_github text,
-    u_articles text,
-    u_questions text,
-    u_answers text,
+    u_id integer primary key comment '用户ID',
+    u_name text comment '用户名',
+    u_psw text comment '密码',
+    u_email text comment '用户email',
+    u_email_confirm integer comment '用户email是否验证',
+    u_level integer comment '用户权限等级',
+    u_reputation integer comment '用户声望',
+    u_realname text comment '真实姓名',
+    u_blog text comment '博客地址',
+    u_github text comment 'github地址',
+    u_articles text comment '文章ID组',
+    u_questions text comment '提问ID组',
+    u_answers text comment '回答ID组',
 );
 
 create table ec_question
 (
-    q_id integer,
-    u_id integer,
-    q_title text,
-    q_text text,
-    q_date date
+    q_id integer comment '问题id',
+    u_id integer comment '提问用户id',
+    q_title text comment '问题标题',
+    q_tag text comment '问题标签',
+    q_text text comment '问题正文',
+    q_date date comment '提问日期',
+    q_close integer comment '是否关闭',
+    q_report integer comment '被举报次数',
 );
 
 create table ec_answer
 (
-    a_id integer primary key,
-    u_id integer,
-    a_text text,
-    a_date date,
-    a_reputation integer
+    a_id integer primary key comment '回答id',
+    u_id integer comment '回答用户id',
+    a_text text comment '回答正文',
+    a_date date comment '回答日期',
+    a_reputation integer comment '回答被赞数',
 
 );
 
 create table ec_article
 (
-    t_id integer primary key,
-    u_id integer,
-    t_title text,
-    t_text text,
-    t_date date,
-    t_reputation integer
-
+    t_id integer primary key comment '文章id',
+    u_id integer comment '文章作者id',
+    t_title text comment '文章标题',
+    t_text text comment '文章正文',
+    t_date date comment '文章发表日期',
+    t_reputation integer comment '文章获得声望',
+    a_like integer comment '文章被赞数',
 );
 
 create table ec_comment
 (
-    c_id integer primary key,
-    t_id integer,
-    u_id integer,
-    c_text text,
-    a_date date
+    c_id integer primary key comment '评论id',
+    type integer comment '评论类型 q/a/t',
+    t_id integer comment '对应事件id',
+    u_id integer comment '评论用户id',
+    c_text text comment '评论正文',
+    c_date date comment '评论日期',
+    c_like date comment '评论被赞数',
 );
 
 create table ec_reputaion
 (
-    r_id integer,
-    type text,
-    id integer,
-    ua_id integer,
-    ub_id integer
+    r_id integer comment '声望变化id',
+    type text comment '声望类型',
+    id integer comment '对应事件id',
+    ua_id integer comment '评价用户id',
+    ub_id integer comment '接受用户id',
 );
-
