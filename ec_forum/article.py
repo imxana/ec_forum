dc03111863ef96bfee6e3ca1a8f5863a1cb4357e
imxana <xana.awaken@gmail.com>
2016-12-02 15:06:59 +0800

    tags no long not existed

--- ec_forum/article.py
+++ ec_forum/article.py
@@ -279,8 +279,8 @@ def run(app):
 
         t_tags_set = set(unpack_id(t_tags)[0])
 
-        if not set(default_tags).issuperset(t_tags_set):
-            return jsonify(error.tagNotExisted)
+        #if not set(default_tags).issuperset(t_tags_set):
+        #    return jsonify(error.tagNotExisted)
 
         '''note: empty is not an error'''
         origin_ids = set()
